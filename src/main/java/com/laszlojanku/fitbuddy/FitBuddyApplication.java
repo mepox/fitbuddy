32ea63f98a3fc883f9623c58fa453d3c2050bd3d
mepox <21198248+mepox@users.noreply.github.com>
2022-10-24 08:01:33 +0100

    fix: disable UserDetailsService auto config

--- src/main/java/com/laszlojanku/fitbuddy/FitBuddyApplication.java
+++ src/main/java/com/laszlojanku/fitbuddy/FitBuddyApplication.java
@@ -2,8 +2,9 @@ package com.laszlojanku.fitbuddy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class FitBuddyApplication {
 
 	public static void main(String[] args) {
