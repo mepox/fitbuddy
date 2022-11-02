b3d0284d560c59f76c62567fd3e740fd8e06456a
mepox <21198248+mepox@users.noreply.github.com>
2022-11-02 11:21:13 +0000

    refactor: remove unused import

--- src/main/java/com/laszlojanku/fitbuddy/dto/RegisterDto.java
+++ src/main/java/com/laszlojanku/fitbuddy/dto/RegisterDto.java
@@ -1,7 +1,6 @@
 package com.laszlojanku.fitbuddy.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
