8cc09d9acfc9050817f9c8ac86a6d1f14d04e9b1
mepox <21198248+mepox@users.noreply.github.com>
2022-11-16 18:15:32 +0000

    Add more annotations for validation to HistoryDto

--- src/main/java/app/fitbuddy/dto/HistoryDto.java
+++ src/main/java/app/fitbuddy/dto/HistoryDto.java
@@ -1,5 +1,10 @@
 package app.fitbuddy.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,8 +14,12 @@ public class HistoryDto {
 	
 	private Integer id;
 	private Integer appUserId;
+	@NotBlank
+	@Size(min = 1, max = 32)
 	private String exerciseName;
+	@PositiveOrZero
 	private Integer weight;
+	@Positive
 	private Integer reps;
 	private String createdOn;	
 
