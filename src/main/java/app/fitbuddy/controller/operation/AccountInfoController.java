177b178b2112f9cdf60c4c41c590b41e802ac213
mepox <21198248+mepox@users.noreply.github.com>
2022-12-01 15:34:48 +0000

    Add @AuthenticationPrincipal to AccountInfoController (#178)

--- src/main/java/app/fitbuddy/controller/operation/AccountInfoController.java
+++ src/main/java/app/fitbuddy/controller/operation/AccountInfoController.java
@@ -4,8 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import app.fitbuddy.dto.accountinfo.AccountInfoResponseDTO;
 import app.fitbuddy.dto.accountinfo.AccountInfoUpdateDTO;
+import app.fitbuddy.security.AppUserPrincipal;
 import app.fitbuddy.service.operation.AccountInfoService;
 
 @RestController
@@ -29,15 +29,14 @@ public class AccountInfoController {
 	}
 	
 	@GetMapping
-	public AccountInfoResponseDTO read() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		return accountInfoService.read(auth.getName());
+	public AccountInfoResponseDTO read(@AuthenticationPrincipal AppUserPrincipal appUserPrincipal) {		
+		return accountInfoService.read(appUserPrincipal.getUsername());
 	}
 	
 	@PutMapping
-	public void update(@RequestBody @Valid AccountInfoUpdateDTO accountInfoUpdateDTO) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		accountInfoService.update(auth.getName(), accountInfoUpdateDTO);				
+	public void update(@RequestBody @Valid AccountInfoUpdateDTO accountInfoUpdateDTO,
+			@AuthenticationPrincipal AppUserPrincipal appUserPrincipal) {		
+		accountInfoService.update(appUserPrincipal.getUsername(), accountInfoUpdateDTO);				
 	}
 
 }
