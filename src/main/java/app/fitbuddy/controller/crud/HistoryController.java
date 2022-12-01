0e186f947d88ba84f9b3111f47325d78042118a0
Mathew Torres <104056426+mtorres6739@users.noreply.github.com>
2022-12-01 01:49:51 -0700

    Add @NotNull to PathVariable in HistoryController (#165)

--- src/main/java/app/fitbuddy/controller/crud/HistoryController.java
+++ src/main/java/app/fitbuddy/controller/crud/HistoryController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Collections;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,75 +34,73 @@ import app.fitbuddy.service.crud.HistoryCrudService;
 @RequestMapping("/user/history")
 @PreAuthorize("authenticated")
 public class HistoryController {
-	
-	private final Logger logger;
-	private final HistoryCrudService historyCrudService;
-	private final AppUserCrudService appUserCrudService;
-	private final String DATE_NOT_VALID = "Date is not valid";
-	
-	@Autowired
-	public HistoryController(HistoryCrudService historyCrudService, AppUserCrudService appUserCrudService) {
-		this.historyCrudService = historyCrudService;
-		this.appUserCrudService = appUserCrudService;
-		this.logger = LoggerFactory.getLogger(HistoryController.class);
-	}
-	
-	@PostMapping
-	public void create(@Valid @RequestBody HistoryRequestDTO historyRequestDTO) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		Integer userId = appUserCrudService.readByName(auth.getName()).getId();
-		if (userId != null) {
-			historyRequestDTO.setAppUserId(userId);
-			historyCrudService.create(historyRequestDTO);
-			logger.info("Creating new history: {}", historyRequestDTO);
-		}
-	}
-	
-	@GetMapping("{date}")
-	public List<HistoryResponseDTO> readAll(@PathVariable("date") String strDate) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (strDate != null) {
-			try {
-				LocalDate.parse(strDate);
-			} catch (DateTimeParseException e) {
-				throw new FitBuddyException(DATE_NOT_VALID);
-			}			
-			Integer userId = appUserCrudService.readByName(auth.getName()).getId();
-			if (userId != null) {
-				List<HistoryResponseDTO> historyResponseDTOs = historyCrudService.readMany(userId, strDate);
-				logger.info("Sending a history for: {}", strDate);
-				return historyResponseDTOs;
-			}
-		}
-		return Collections.emptyList();
-	}
-	
-	@PutMapping("{id}")
-	public void update(@PathVariable("id") Integer historyId, @Valid @RequestBody HistoryUpdateDTO historyUpdateDTO) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (historyId != null) {
-			Integer userId = appUserCrudService.readByName(auth.getName()).getId();
-			if (userId != null) {
-				historyCrudService.update(historyId, historyUpdateDTO);
-				logger.info("Updating history: {}", historyUpdateDTO);				
-			}
-		}
-	}
-	
-	@DeleteMapping("{id}")
-	public void delete(@PathVariable("id") Integer historyId) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (historyId != null) {
-			Integer userId = appUserCrudService.readByName(auth.getName()).getId();
-			if (userId != null) {
-				HistoryResponseDTO historyResponseDTO = historyCrudService.readById(historyId);				
-				if (historyResponseDTO != null && historyResponseDTO.getAppUserId().equals(userId)) {
-					historyCrudService.delete(historyId);
-					logger.info("Deleting history: {}", historyResponseDTO);
-				} else {
-					throw new FitBuddyException("UserIds doesn't match. Cannot delete others History.");
-				}
-			}
-		}	
-	}
+
+    private final Logger logger;
+    private final HistoryCrudService historyCrudService;
+    private final AppUserCrudService appUserCrudService;
+    private final String DATE_NOT_VALID = "Date is not valid";
+
+    @Autowired
+    public HistoryController(HistoryCrudService historyCrudService, AppUserCrudService appUserCrudService) {
+        this.historyCrudService = historyCrudService;
+        this.appUserCrudService = appUserCrudService;
+        this.logger = LoggerFactory.getLogger(HistoryController.class);
+    }
+
+    @PostMapping
+    public void create(@Valid @RequestBody HistoryRequestDTO historyRequestDTO) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = appUserCrudService.readByName(auth.getName()).getId();
+        if (userId != null) {
+            historyRequestDTO.setAppUserId(userId);
+            historyCrudService.create(historyRequestDTO);
+            logger.info("Creating new history: {}", historyRequestDTO);
+        }
+    }
+
+    @GetMapping("{date}")
+    public List<HistoryResponseDTO> readAll(@PathVariable("date") String strDate) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (strDate != null) {
+            try {
+                LocalDate.parse(strDate);
+            } catch (DateTimeParseException e) {
+                throw new FitBuddyException(DATE_NOT_VALID);
+            }
+            Integer userId = appUserCrudService.readByName(auth.getName()).getId();
+            if (userId != null) {
+                List<HistoryResponseDTO> historyResponseDTOs = historyCrudService.readMany(userId, strDate);
+                logger.info("Sending a history for: {}", strDate);
+                return historyResponseDTOs;
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") @NotNull Integer historyId, @Valid @RequestBody HistoryUpdateDTO historyUpdateDTO) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = appUserCrudService.readByName(auth.getName()).getId();
+        if (userId != null) {
+            historyCrudService.update(historyId, historyUpdateDTO);
+            logger.info("Updating history: {}", historyUpdateDTO);
+        }
+
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") @NotNull Integer historyId) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Integer userId = appUserCrudService.readByName(auth.getName()).getId();
+        if (userId != null) {
+            HistoryResponseDTO historyResponseDTO = historyCrudService.readById(historyId);
+            if (historyResponseDTO != null && historyResponseDTO.getAppUserId().equals(userId)) {
+                historyCrudService.delete(historyId);
+                logger.info("Deleting history: {}", historyResponseDTO);
+            } else {
+                throw new FitBuddyException("UserIds doesn't match. Cannot delete others History.");
+            }
+        }
+
+    }
 }
