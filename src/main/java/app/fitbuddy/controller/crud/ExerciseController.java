58ce96a930bc2adb4db2e824dff475a58f3753f4
Ravi Mandalia <ravimandalia10@gmail.com>
2022-12-03 12:54:36 +0530

    Remove userId null checks in ExerciseController (#179)

--- src/main/java/app/fitbuddy/controller/crud/ExerciseController.java
+++ src/main/java/app/fitbuddy/controller/crud/ExerciseController.java
@@ -1,31 +1,21 @@
 package app.fitbuddy.controller.crud;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import app.fitbuddy.dto.exercise.ExerciseRequestDTO;
 import app.fitbuddy.dto.exercise.ExerciseResponseDTO;
 import app.fitbuddy.dto.exercise.ExerciseUpdateDTO;
 import app.fitbuddy.exception.FitBuddyException;
 import app.fitbuddy.security.AppUserPrincipal;
 import app.fitbuddy.service.crud.ExerciseCrudService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/exercises")
@@ -45,22 +35,17 @@ public class ExerciseController {
 	public void create(@RequestBody @Valid ExerciseRequestDTO exerciseRequestDTO,
 			@AuthenticationPrincipal AppUserPrincipal appUserPrincipal) {
 		Integer userId = appUserPrincipal.getId();
-		if (userId != null) {	
-			exerciseRequestDTO.setAppUserId(userId);
-			exerciseCrudService.create(exerciseRequestDTO);
-			logger.info("Creating new exercise: {}", exerciseRequestDTO);
-		}
+		exerciseRequestDTO.setAppUserId(userId);
+		exerciseCrudService.create(exerciseRequestDTO);
+		logger.info("Creating new exercise: {}", exerciseRequestDTO);
 	}
 
 	@GetMapping
 	public List<ExerciseResponseDTO> readAll(@AuthenticationPrincipal AppUserPrincipal appUserPrincipal) {		
 		Integer userId = appUserPrincipal.getId();
-		if (userId != null) {
-			List<ExerciseResponseDTO> responseDTOs = exerciseCrudService.readMany(userId);
-			logger.info("Sending a list of exercises: {}", responseDTOs);
-			return responseDTOs;
-		}
-		return Collections.emptyList();
+		List<ExerciseResponseDTO> responseDTOs = exerciseCrudService.readMany(userId);
+		logger.info("Sending a list of exercises: {}", responseDTOs);
+		return responseDTOs;
 	}
 	
 	@PutMapping("{id}")
@@ -68,14 +53,12 @@ public class ExerciseController {
 			@Valid @RequestBody ExerciseUpdateDTO exerciseUpdateDTO,
 			@AuthenticationPrincipal AppUserPrincipal appUserPrincipal) {		
 		Integer userId = appUserPrincipal.getId();
-		if (userId != null) {
-			ExerciseResponseDTO exerciseResponseDTO = exerciseCrudService.readById(exerciseId);
-			if (exerciseResponseDTO != null && exerciseResponseDTO.getAppUserId().equals(userId)) {
-				exerciseCrudService.update(exerciseId, exerciseUpdateDTO);
-				logger.info("Updating the exercise: {}", exerciseUpdateDTO);
-			} else {
-				throw new FitBuddyException("UserIds doesn't match. Cannot update others Exercise.");
-			}
+		ExerciseResponseDTO exerciseResponseDTO = exerciseCrudService.readById(exerciseId);
+		if (exerciseResponseDTO != null && exerciseResponseDTO.getAppUserId().equals(userId)) {
+			exerciseCrudService.update(exerciseId, exerciseUpdateDTO);
+			logger.info("Updating the exercise: {}", exerciseUpdateDTO);
+		} else {
+			throw new FitBuddyException("UserIds doesn't match. Cannot update others Exercise.");
 		}
 	}
 	
@@ -83,14 +66,12 @@ public class ExerciseController {
 	public void delete(@PathVariable("id") @NotNull Integer exerciseId,
 			@AuthenticationPrincipal AppUserPrincipal appUserPrincipal) {
 		Integer userId = appUserPrincipal.getId();
-		if (userId != null) {
-			ExerciseResponseDTO exerciseResponseDTO = exerciseCrudService.readById(exerciseId);
-			if (exerciseResponseDTO != null && exerciseResponseDTO.getAppUserId().equals(userId)) {
-				exerciseCrudService.delete(exerciseId);
-				logger.info("Deleting exercise: {}", exerciseResponseDTO);
-			} else {
-				throw new FitBuddyException("UserIds doesn't match. Cannot delete others Exercise.");
-			}
+		ExerciseResponseDTO exerciseResponseDTO = exerciseCrudService.readById(exerciseId);
+		if (exerciseResponseDTO != null && exerciseResponseDTO.getAppUserId().equals(userId)) {
+			exerciseCrudService.delete(exerciseId);
+			logger.info("Deleting exercise: {}", exerciseResponseDTO);
+		} else {
+			throw new FitBuddyException("UserIds doesn't match. Cannot delete others Exercise.");
 		}
 	}
 
